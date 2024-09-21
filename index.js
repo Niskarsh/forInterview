107311aa4e5e084ad12432cac39c8e2b93a2c507
Niskarsh <niskarshk@gmail.com>
2024-09-21 16:21:28 +0530

    added code

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const faker = require('faker');
+const { v4: uuidv4 } = require('uuid');
+const { data1 } = require('./data1');
+const { data2 } = require('./data2');
+const { data3 } = require('./data3');
+require('dotenv').config();
+const app = express()
+
+let people_data = [];
+
+app.get('/', function (req, res) {
+    res.send('ok')
+})
+
+// Return all people data without their data attribute
+app.get('/people', function (req, res) {
+    let people = people_data.map(person => {
+        let { data, ...rest } = person;
+        return rest;
+    });
+    res.json(people);
+})
+
+// Route to return people data with their data attribute by id
+app.get('/people/:id', function (req, res) {
+    let id = req.params.id;
+    let person = people_data.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).json({ message: 'Person not found' });
+    }
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    let dataArray = [ data1, data2, data3, data1, data3, data2, data3, data2, data1, data3, data1, data2, data2, data1, data3 ];
+
+    for (let i =0 ; i<15; i++) {
+        let person = {
+            id: uuidv4(),
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            phone: faker.phone.phoneNumber(),
+            address: faker.address.streetAddress(),
+            bio: faker.lorem.sentence(),
+            data: dataArray[i],
+        }
+        people_data.push(person);
+    }
+});
