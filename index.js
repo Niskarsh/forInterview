1adec5bd93b7aa0e867c3b9f2248f153e22bbc14
Niskarsh <niskarshk@gmail.com>
2024-09-21 16:57:55 +0530

    cors

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ let people_data = [];
 app.get('/', function (req, res) {
     res.send('ok')
 })
+// Add middlware to enable CORS on all domains
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
 
 // Return all people data without their data attribute
 app.get('/people', function (req, res) {
